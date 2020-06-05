40290ef4fbe63607de3640ee38b82243979dc2b1
Eric Biggers <ebiggers@google.com>
2020-06-05 16:51:47 -0700

    scripts/do-release.sh: clean up to pass shellcheck
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -15,16 +15,16 @@ PKG=fsverity-utils-$VERS
 sed -E -i \
 	"/\#define PACKAGE_VERSION/s/v[0-9]+(\.[0-9]+)*(-[a-z0-9]+)?/v$VERS/" \
 	fsverity.c
-git commit -a --signoff --message=v$VERS
-git tag --sign v$VERS --message=$PKG
+git commit -a --signoff --message="v$VERS"
+git tag --sign "v$VERS" --message="$PKG"
 
-git archive v$VERS --prefix=$PKG/ > $PKG.tar
-rm -rf $PKG
-tar xf $PKG.tar
-( cd $PKG && make all )
-rm -r $PKG
+git archive "v$VERS" --prefix="$PKG/" > "$PKG.tar"
+rm -rf "$PKG"
+tar xf "$PKG.tar"
+( cd "$PKG" && make all )
+rm -r "$PKG"
 
-gpg --detach-sign --armor $PKG.tar
+gpg --detach-sign --armor "$PKG.tar"
 DESTDIR=/pub/linux/kernel/people/ebiggers/fsverity-utils/v$VERS
-kup mkdir $DESTDIR
-kup put $PKG.tar $PKG.tar.asc $DESTDIR/$PKG.tar.gz
+kup mkdir "$DESTDIR"
+kup put "$PKG.tar" "$PKG.tar.asc" "$DESTDIR/$PKG.tar.gz"
